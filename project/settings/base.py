f5dd91fdbaf2087a906103a30d7c7a59302512ab
Alexander Sapronov <a@sapronov.me>
2016-02-19 00:24:59 +0600

    Добавил деплой конфиги

--- project/settings/base.py
+++ project/settings/base.py
@@ -28,7 +28,7 @@ def get_secret(setting, secrets=secrets):
 # See https://docs.djangoproject.com/en/1.8/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'x%_u_thw2(q-@clt0=*w2t4f*25_w3e1--a=m8^bg8*bsxf8w!'
+SECRET_KEY = get_secret('secret_key')
 SITE_TITLE = 'Python тесты'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
