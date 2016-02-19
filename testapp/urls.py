12ca1f63e9386feb4f86e5749d6527be43f804a3
Alexander Sapronov <a@sapronov.me>
2016-02-19 16:02:21 +0600

    Добавил страницу с донатом

--- testapp/urls.py
+++ testapp/urls.py
@@ -1,13 +1,15 @@
 # -*- encoding: utf-8 -*-
 from django.conf.urls import patterns, url
 
-from testapp.views import ThanksPage, AboutPage, IndexPage, RatingPage, HowToWorkPage, TestsPage, TestDetailPage
+from testapp.views import ThanksPage, AboutPage, IndexPage, RatingPage, HowToWorkPage, TestsPage, TestDetailPage, \
+    DonatePage
 
 urlpatterns = patterns(
     '',
     url(r'^thanks$', ThanksPage.as_view(), name='thanks'),
     url(r'^about$', AboutPage.as_view(), name='about'),
     url(r'^rating$', RatingPage.as_view(), name='rating'),
+    url(r'^donate$', DonatePage.as_view(), name='donate'),
     url(r'^howto$', HowToWorkPage.as_view(), name='howtowork'),
 
     url(r'^tests$', TestsPage.as_view(), name='tests'),
--- testapp/views.py
+++ testapp/views.py
@@ -1,8 +1,9 @@
 # -*- encoding: utf-8 -*-
-from django.views.generic import TemplateView
 from django.views.generic import ListView, DetailView
+from django.views.generic import TemplateView
 
 from testapp.models import Test
+
 
 class ThanksPage(TemplateView):
     template_name = 'pages/thanks.html'
@@ -24,9 +25,12 @@ class TestDetailPage(DetailView):
     model = Test
 
 
-
 class RatingPage(TemplateView):
     template_name = 'pages/rating.html'
+
+
+class DonatePage(TemplateView):
+    template_name = 'pages/donate.html'
 
 
 class IndexPage(TemplateView):
