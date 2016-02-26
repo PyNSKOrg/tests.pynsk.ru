c5aa3436eef1ffaf55866803cf9fc3a4689b8c0d
Alexander Sapronov <sapronov.alexander92@gmail.com>
2016-02-26 13:15:43 +0600

    Merge

--- testapp/exceptions.py
+++ testapp/exceptions.py
@@ -1,12 +1,18 @@
-# -*- encoding: utf-8 -*-
-from django.utils.translation import ugettext as _
-
-
 class TestAppError(Exception):
     def __str__(self):
-        return _('Error in test application')
+        return "Error in test application"
 
 
 class OpenGameAlreadyExists(TestAppError):
     def __str__(self):
-        return _('Player already has open game.')
+        return "Player already has open game."
+
+
+class TestIsNotAvailable(TestAppError):
+    def __init__(self, available_time=None):
+        self.available_time = available_time
+
+    def __str__(self):
+        return "Test is not available."
+
+
--- testapp/models.py
+++ testapp/models.py
@@ -1,11 +1,11 @@
-# -*- encoding: utf-8 -*-
-from django.core.exceptions import MultipleObjectsReturned
+from __future__ import division
 from django.db import models
+from django.db.models import F
 from django.utils import timezone
-from django.utils.translation import ugettext as _
-
-from testapp.managers import GameManager, \
-    GameQuestionManager, PlayerManager, TestManager
+from datetime import timedelta
+from testapp.sql import create_testapp_resultview
+from testapp.managers import GameManager, PlayerManager, TestManager, GameQuestionManager, ResultViewManager
+from testapp.exceptions import TestIsNotAvailable
 
 
 def gen_letter():
@@ -15,59 +15,76 @@ def gen_letter():
         yield char
 
 
+def calculate_result_score(total_questions, right_answers):
+    if total_questions == 0:
+        return 0
+    return round((right_answers / total_questions) * 5, 2)
+
+
 class Question(models.Model):
     OPEN = 'O'
     CLOSE = 'C'
-    QTYPE = ((OPEN, _('open question')), (CLOSE, _('close question')),)
-
+    QTYPE = (
+        (OPEN, "open question"),
+        (CLOSE, "close question"),
+    )
     class Meta:
-        verbose_name = _('Question')
-        verbose_name_plural = 'Questions'
-
-    text = models.CharField(_('Question text'), max_length=2000)
-    created_on = models.DateTimeField(_('When was created'), auto_now_add=True)
-    changed_on = models.DateTimeField(_('When was last changed'), auto_now=True)
-    published = models.BooleanField(_('is published'), default=True)
-    qtype = models.CharField(_('question type'),
-                             choices=QTYPE,
-                             max_length=10,
-                             default=CLOSE)
-
-    def __str__(self):
-        return u"%s" % self.text
+        verbose_name = "Question"
+        verbose_name_plural = "Questions"
+    text = models.CharField("Question text", max_length=2000)
+    created_on = models.DateTimeField("When was created", auto_now_add=True)
+    changed_on = models.DateTimeField("When was last changed ", auto_now=True)
+    published = models.BooleanField("is published", default=True)
+    qtype = models.CharField("question type", choices=QTYPE, max_length=10, default=CLOSE)
 
     def __unicode__(self):
-        return u"%s" % self.text
+        return u"%s" % (self.text)
 
 
 class Test(models.Model):
-    name = models.CharField(_('Test name'), max_length=200)
-    created_on = models.DateTimeField(_('When was created'), auto_now_add=True)
-    changed_on = models.DateTimeField(_('When was last changed'), auto_now=True)
+    name = models.CharField("Test name", max_length=200)
+    created_on = models.DateTimeField("When was created", auto_now_add=True)
+    changed_on = models.DateTimeField("When was last changed ", auto_now=True)
     questions = models.ManyToManyField(Question, through='TestQuestion')
-    published = models.BooleanField(_('is published'), default=False)
-    author = models.CharField(_('Author'), max_length=200, blank=True)
-    description = models.CharField(_('Description'), max_length=2000, blank=True)
+    published = models.BooleanField("is published", default=False)
+    author = models.CharField("Author", max_length=200, blank=True)
+    description = models.CharField("Description", max_length=2000, blank=True)
+    interval = models.PositiveIntegerField("Retake interval in seconds", default=0)
     objects = models.Manager()
     manager = TestManager()
 
+    def get_interval(self):
+        tdelta = timedelta(seconds=self.interval)
+        result = {"day": tdelta.days}
+        result["hour"], rem = divmod(tdelta.seconds, 3600)
+        result["minute"], result["second"] = divmod(rem, 60)
+        return result
+
+    def check_availability(self, player):
+        if not self.published:
+            raise TestIsNotAvailable()
+        last_game = self.games.filter(player=player).order_by("-start_on").first()
+        if not last_game:
+            return True
+        if last_game.state == Game.OPEN:
+            raise TestIsNotAvailable()
+        interval = timedelta(seconds=self.interval)
+        available_time = last_game.stop_on + interval
+        if timezone.now() > available_time:
+            return True
+        else:
+            raise TestIsNotAvailable(available_time=available_time)
+
+
     def __unicode__(self):
-        return u"%s" % self.name
+        return u"%s" % (self.name)
 
 
 class Player(models.Model):
-    tgm_user_id = models.CharField(_('Telegram user id'),
-                                   max_length=2000,
-                                   unique=True)
-    tgm_first_name = models.CharField(_("Telegram user's first name"),
-                                      max_length=2000,
-                                      blank=True)
-    tgm_last_name = models.CharField(_("Telegram user's last name"),
-                                     max_length=2000,
-                                     blank=True)
-    tgm_user_name = models.CharField(_('Telegram username'),
-                                     max_length=2000,
-                                     blank=True)
+    tgm_user_id = models.CharField("Telegram user id", max_length=2000, unique=True)
+    tgm_first_name = models.CharField("Telegram user's first name", max_length=2000, blank=True)
+    tgm_last_name = models.CharField("Telegram user's last name", max_length=2000, blank=True)
+    tgm_user_name = models.CharField("Telegram username", max_length=2000, blank=True)
     tests = models.ManyToManyField(Test, through='Game')
     objects = models.Manager()
     manager = PlayerManager()
@@ -78,69 +95,71 @@ class Player(models.Model):
             game.stop()
 
     def current_game(self):
-        """Return open game.
-
-        If multiple return last open game. Otherwise return None.
-
+        """
+        Return open game. If multiple return last open game.
+        Otherwise return None.
         """
         try:
             return self.games.get(state=Game.OPEN)
         except Game.DoesNotExist:
             return None
-        except MultipleObjectsReturned:
-            return self.games.filter(
-                    state=Game.OPEN).order_by('-start_on').first()
+        except Game.MultipleObjectsReturned:
+            return self.games.filter(state=Game.OPEN).order_by('-start_on').first()
+
+    def get_rating(self):
+        rating = []
+        all_result = ResultView.objects.all()
+        player_result_list = [
+            result for result in all_result if result.player == self
+        ]
+        for player_result in player_result_list:
+            worse_result = 0
+            all_test_result = [ res for res in all_result if res.test == player_result.test ]
+            for result in all_test_result:
+                if result.score() < player_result.score():
+                    worse_result += 1
+            rating.append((
+                player_result,
+                round((worse_result / len(all_test_result)) * 100, 2)))
+        return rating
+
 
     def __unicode__(self):
-        return u"%s" % self.tgm_user_id
-
-    def __str__(self):
-        return u"%s" % self.tgm_user_id
+        return u"%s, %s, %s, %s" % (self.tgm_user_id, self.tgm_last_name, self.tgm_first_name, self.tgm_user_name)
 
 
 class TestQuestion(models.Model):
     class Meta:
-        unique_together = (('test', 'question'),)
-
+        unique_together = (("test", "question"),)
     test = models.ForeignKey('Test')
     question = models.ForeignKey('Question')
-
     def __unicode__(self):
-        return u"Test: %s. Question: %s" % (self.test, self.question)
-
-    def __str__(self):
         return u"Test: %s. Question: %s" % (self.test, self.question)
 
 
 class Answer(models.Model):
     MAX_LENGTH = 200
-    question = models.ForeignKey(_('Question'), related_name='answers')
-    text = models.CharField(_('Answer text'), max_length=MAX_LENGTH)
-    answer_description = models.TextField(_('Answer description'),
-                                          max_length=20000,
-                                          blank=True)
-    is_true = models.BooleanField(_('is true'), default=False)
-
-    def __str__(self):
-        return u"%s" % self.text
-
+    question = models.ForeignKey('Question', related_name='answers')
+    text = models.CharField("Answer text", max_length=MAX_LENGTH)
+    answer_description = models.TextField("Answer description", max_length=20000, blank=True)
+    is_true = models.BooleanField("is true", default=False)
     def __unicode__(self):
-        return u"%s" % self.text
+        return u"%s" % (self.text)
 
 
 class Game(models.Model):
     OPEN = 'O'
     CLOSE = 'C'
-    STATE = ((OPEN, _('Open game')), (CLOSE, _('Close game')),)
-    player = models.ForeignKey('Player', related_name='games')
-    test = models.ForeignKey('Test')
-    start_on = models.DateTimeField(_('when was started'), auto_now_add=True)
-    stop_on = models.DateTimeField(_('when was stoped'), null=True, blank=True)
-    questions = models.ManyToManyField(_('Question'), through='GameQuestion')
-    state = models.CharField(_('Game state'),
-                             choices=STATE,
-                             max_length=10,
-                             default=OPEN)
+    STATE = (
+        (OPEN, 'Open game'),
+        (CLOSE, 'Close game'),
+    )
+    player = models.ForeignKey("Player", related_name='games')
+    test = models.ForeignKey('Test', related_name='games')
+    start_on = models.DateTimeField("when was started", auto_now_add=True)
+    stop_on = models.DateTimeField("when was stoped", null=True, blank=True)
+    questions = models.ManyToManyField("Question", through='GameQuestion')
+    state = models.CharField("Game state", choices=STATE, max_length=10, default=OPEN)
     objects = models.Manager()
     manager = GameManager()
 
@@ -152,8 +171,7 @@ class Game(models.Model):
 
     def no_more_questions(self):
         asked_questions = self.questions.all()
-        not_asked_questions = self.test.questions.filter(
-                published=True).exclude(id__in=asked_questions)
+        not_asked_questions = self.test.questions.filter(published=True).exclude(id__in=asked_questions)
         if not_asked_questions:
             return False
         else:
@@ -165,8 +183,7 @@ class Game(models.Model):
         number = None
         if self.state == Game.OPEN:
             asked_questions = self.questions.all()
-            not_asked_questions = self.test.questions.filter(
-                    published=True).exclude(id__in=asked_questions)
+            not_asked_questions = self.test.questions.filter(published=True).exclude(id__in=asked_questions)
             question = not_asked_questions.order_by('?').first()
             if question:
                 number = len(asked_questions) + 1
@@ -181,12 +198,11 @@ class Game(models.Model):
                 game_question.save()
             else:
                 self.stop()
-        return question, answers, number
+        return (question, answers, number)
 
     def current_game_question(self):
         try:
-            game_question = self.game_questions.order_by('-asked_at')[0:1].get(
-            )
+            game_question = self.game_questions.order_by('-asked_at')[0:1].get()
             return game_question
         except GameQuestion.DoesNotExist:
             return None
@@ -194,37 +210,29 @@ class Game(models.Model):
     def result(self):
         result = {
             'state': self.state,
-            'total_questions':
-                self.test.questions.filter(published=True).count(),
+            'total_questions': self.test.questions.filter(published=True).count(),
             'asked_questions': self.game_questions.count(),
-            'right_answers':
-                GameQuestion.objects.right_answers(game=self).count(),
+            'right_answers': GameQuestion.objects.right_answers(game=self).count(),
         }
+        result['score'] = calculate_result_score(
+            result['total_questions'], result['right_answers']
+        )
         return result
 
     def __unicode__(self):
-        return u"%s Test: %s. Player: %s" % (self.start_on, self.test,
-                                             self.player)
-
-    def __str__(self):
-        return u"%s Test: %s. Player: %s" % (self.start_on, self.test,
-                                             self.player)
+        return u"%s Test: %s. Player: %s" % (self.start_on, self.test, self.player)
 
 
 class GameQuestion(models.Model):
     class Meta:
-        unique_together = (('game', 'question'),)
+        unique_together = (("game", "question"),)
 
-    game = models.ForeignKey('Game', related_name='game_questions')
-    question = models.ForeignKey('Question')
-    right_answer = models.CharField(_('Letter of right answer'),
-                                    blank=True,
-                                    max_length=1)
-    player_answer = models.CharField(_('Player answer'), blank=True, max_length=1)
-    asked_at = models.DateTimeField(_('When was asked'), auto_now_add=True)
-    answered_at = models.DateTimeField(_('When was answered'),
-                                       null=True,
-                                       blank=True)
+    game = models.ForeignKey("Game", related_name="game_questions")
+    question = models.ForeignKey("Question")
+    right_answer = models.CharField("Letter of right answer", blank=True, max_length=1)
+    player_answer = models.CharField("Player answer", blank=True, max_length=1)
+    asked_at = models.DateTimeField("When was asked", auto_now_add=True)
+    answered_at = models.DateTimeField("When was answered", null=True, blank=True)
     objects = GameQuestionManager()
 
     def reply(self, answer):
@@ -248,5 +256,31 @@ class GameQuestion(models.Model):
     def __unicode__(self):
         return u"%s, %s" % (self.game, self.question)
 
-    def __str__(self):
-        return u"%s, %s" % (self.game, self.question)
+
+class ResultView(models.Model):
+    """Model presenting "testapp_result" postgresql view"""
+    SQL = create_testapp_resultview
+    class Meta:
+        managed = False
+    id = models.PositiveIntegerField(primary_key=True)
+    player = models.ForeignKey(Player)
+    test = models.ForeignKey(Test)
+    test_name = models.CharField("Test name", max_length=200)
+    number_of_questions = models.PositiveIntegerField()
+    number_of_right_answers = models.PositiveIntegerField()
+    objects = ResultViewManager()
+
+    def score(self):
+        return calculate_result_score(
+            self.number_of_questions,
+            self.number_of_right_answers
+        )
+
+    def delete(self, *args, **kwargs):
+        raise NotImplementedError
+
+    def save(self, *args, **kwargs):
+        raise NotImplementedError
+
+
+
